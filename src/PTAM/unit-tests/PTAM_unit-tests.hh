6caeefc2583fbb259df427d29a0a3a6befe36b7c
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2023-02-13 14:22:50 -0500

    added PTAM module

---
+++ src/PTAM/unit-tests/PTAM_unit-tests.hh
@@ -0,0 +1,15 @@
+/*
+#include"../temp_access_mem.hpp"
+#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
+
+
+void testOutput(char* method,char* result, bool status);
+
+void testValueAddRetrieveCHAR();
+
+void testValueAddRetrieveFLOAT();
+
+void testValueAddRetrieveINT();
+*/
