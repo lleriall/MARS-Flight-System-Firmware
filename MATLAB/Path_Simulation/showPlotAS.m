8d784efdb3556bcf9672f37633d36e88d6d26127
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2023-05-10 17:21:16 -0400

    rm path app

--- MATLAB/Path_Simulation/showPlotAS.m
+++
@@ -1,9 +0,0 @@
-function showPlotAS(checkboxValue,array)%<SM:PDF>%<SM:PDF_PARAM>
-    if checkboxValue == 0 
-        figure(2)
-        plot(array);
-    else %<SM:IF>
-        figure(2)
-        plot(array,'.'); 
-    end
-end
