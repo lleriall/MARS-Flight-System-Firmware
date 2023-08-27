6c4faf472b3f207863664058d00d808e083a3465
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2023-08-27 05:16:05 -0400

    web emc controller interfce now works + battery driver additions

--- base-firmware/components/HALX/mg90s_servo.h
+++ base-firmware/components/HALX/mg90s_servo.h
@@ -28,13 +28,18 @@ SOFTWARE.*/
 #define SPEED_DEFAULT 1
 #define SPEED_FAST 20
 
+#define SERVO_FL 0
+#define SERVO_FR 1
+#define SERVO_RL 2
+#define SERVO_RR 3
+
 class WingTranslate {
     public:
         static void mcpwm_gpio_initialize(uint8_t motor);
 
         static uint32_t servo_per_degree_init(uint32_t degree_of_rotation);
         
-        uint8_t mcpwm_servo_control(uint32_t angle,uint8_t pin, uint8_t speed);
+        uint8_t mcpwm_servo_control(uint32_t angle,uint8_t pin);
 
     private:
         uint8_t GET_SERVO_POS(uint8_t pin);
